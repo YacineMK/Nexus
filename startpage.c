9090961c8222ddae43c79afe1bb726d5028d6fca
YacineMK <merroukiyacine06@gmail.com>
2023-10-30 12:04:23 +0100

    finish home page

--- startpage.c
+++ startpage.c
@@ -12,6 +12,26 @@ void cleanup(SDL_Window *win, SDL_Renderer *renderer) {
     IMG_Quit();
 }
 
+void btn(SDL_Renderer *renderer, TTF_Font *font, const char* text, int h) {
+    int buttonWidth = 0.6 * 800;
+    int buttonHeight = 60;
+    int buttonX = (800 - buttonWidth) / 2;
+    int buttonY = h - buttonHeight; 
+    SDL_Rect buttonRect = {buttonX, buttonY, buttonWidth, buttonHeight};
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &buttonRect);
+
+    SDL_Color buttonTextColor = {37, 37, 37};
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, buttonTextColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+    SDL_Rect buttonTextRect = { buttonX + (buttonWidth - textSurface->w) / 2, buttonY + (buttonHeight - textSurface->h) / 2, textSurface->w, textSurface->h};
+    SDL_RenderCopy(renderer, textTexture, NULL, &buttonTextRect);
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
+}
+
+
+
 int main() {
     SDL_Window *win;
     SDL_Renderer *renderer;
@@ -26,13 +46,13 @@ int main() {
         SDL_Quit();
         return 1;
     }
-
+    
     win = SDL_CreateWindow(
         "Memo with Smaili",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         800,
-        800,
+        700,
         SDL_WINDOW_SHOWN
     );
 
@@ -56,7 +76,7 @@ int main() {
         return 1;
     }
 
-    TTF_Font *font = TTF_OpenFont("./assets/Terminal.ttf", 40);
+    TTF_Font *font = TTF_OpenFont("./assets/Terminal.ttf", 41);
     if (font == NULL) {
         printf("TTF_OpenFont() failed: %s\n", TTF_GetError());
         cleanup(win, renderer);
@@ -70,7 +90,7 @@ int main() {
     SDL_Event exit;
 
     SDL_Texture *backgroundTexture = NULL;
-    SDL_Surface *backgroundSurface = IMG_Load("./assets/bg_purple.png");
+    SDL_Surface *backgroundSurface = IMG_Load("./assets/bg.png");
 
     if (backgroundSurface == NULL) {
         printf("Failed to load background image: %s\n", IMG_GetError());
@@ -92,22 +112,25 @@ int main() {
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 37, 37, 37, 255);
-        SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 37, 37, 37, 255);
+            SDL_RenderClear(renderer);
 
-        // Render the background image
-        SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
+            SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
 
-        // Render text
-        SDL_Color textColor = {255, 255, 255}; // White text color
-        textSurface = TTF_RenderText_Solid(font, "Welcome to Memo with Smaili", textColor);
-        textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-        SDL_Rect textRect = {100, 100, textSurface->w, textSurface->h};
-        SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+            SDL_Color textColor = {255, 255, 255};
+            textSurface = TTF_RenderText_Solid(font, "Welcome to Memo with Smaili", textColor);
+            textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+            SDL_Rect textRect = {120, 120, textSurface->w, textSurface->h};
+            SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
 
-        SDL_RenderPresent(renderer);
+            btn(renderer, font, "Start" , 420);
+            btn(renderer, font, "Help" , 510);
+            btn(renderer, font, "High Score" , 600);
+            SDL_FreeSurface(textSurface);
+            SDL_DestroyTexture(textTexture);
+            SDL_RenderPresent(renderer); 
     }
-
+    
     SDL_FreeSurface(textSurface);
     SDL_DestroyTexture(textTexture);
     TTF_CloseFont(font);
